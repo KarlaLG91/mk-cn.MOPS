ad650a7c3fb81aee38297e1e21606529d470c4dc
Karla91 <klg1219sh@gmail.com>
2018-04-24 17:37:51 -0500

    Se modifica bin/cnmops.R haciendo las variables mas descriptivas.

--- bin/cnmops.R
+++ bin/cnmops.R
@@ -3,18 +3,18 @@ args = commandArgs(trailingOnly = TRUE)
 #FOR DEBUGGING ONLY
 #Reading the entry values dor the paths to input file and output file.
 #args[1] is the path to the input BAM files
-#args[1] <- "/home/consultorwg/mk-cn.MOPS/data/test/"
+#args[1] <- "/data/test_cnmops/"
 #args[2] is the path to the input bed file
-#args[2] <- "/home/consultorwg/mk-cn.MOPS/data/039970_D_BED_20120404_pad50.bed"
+#args[2] <- "/reference/039970_D_BED_20120404_pad50.bed"
 #args[3] is the path to the output file with the .csv file.
-#args[3] <- "results/test.csv"
+#args[3] <- "results/test_cnmops/cnv_results.csv"
 
 library(cn.mops)
 BAMFiles <- list.files(path= args[1], pattern=".bam$", full.names = TRUE)
-segments <- read.table(args[2], sep="\t", as.is=TRUE)
-gr <- GRanges(segments[,1],IRanges(segments[,2],segments[,3]))
-X <- getSegmentReadCountsFromBAM(BAMFiles,GR=gr)
-resCNMOPS <- exomecn.mops(X)
-resCNMOPS <- calcIntegerCopyNumbers(resCNMOPS)
-CNVs <- as.data.frame(cnvs(resCNMOPS))
-write.csv(CNVs,file= args[3])
+BEDFile <- read.table(args[2], sep="\t", as.is=TRUE)
+genomic_ranges <- GRanges(BEDFile[,1],IRanges(BEDFile[,2],BEDFile[,3]))
+Read_counts <- getSegmentReadCountsFromBAM(BAMFiles,GR=gr)
+resCNMOPS <- exomecn.mops(Read_counts)
+IntegerCN <- calcIntegerCopyNumbers(resCNMOPS)
+CNVs <- as.data.frame(cnvs(IntegerCN))
+write.table(CNVs,file= args[3], quote=FALSE, sep= "\t", row.names=FALSE)
