0fb710a520b33f9cee70791bd300bca74acb7d56
Karla91 <klg1219sh@gmail.com>
2018-04-25 12:02:51 -0500

    Se agregó header a script cnmops.R y se comentó cada paso para hacerlo más descriptivo.

--- bin/cnmops.R
+++ bin/cnmops.R
@@ -1,20 +1,40 @@
-#Reading arguments from shell
-args = commandArgs(trailingOnly = TRUE)
-#FOR DEBUGGING ONLY
-#Reading the entry values dor the paths to input file and output file.
-#args[1] is the path to the input BAM files
-#args[1] <- "/data/test_cnmops/"
-#args[2] is the path to the input bed file
-#args[2] <- "/reference/039970_D_BED_20120404_pad50.bed"
-#args[3] is the path to the output file with the .csv file.
-#args[3] <- "results/test_cnmops/cnv_results.csv"
+## DESCRIPTION:
+## R script to run cn.MOPS algorithm in order to determine CNVs in multiple samples.
+## The script uses BAM files to obtain the read counts, and a BED file to establish segment regions.
+## The script also generates a tab delimited file with the copy number clases found on each sample.
+##
+## USAGE:
+## This R script is called by the mkfile of this module with the following command:
+##	`Rscript --vanilla bin/cnmops.R <SPECIFIC _ARGUMENT_1> <SPECIFIC _ARGUMENT_2> <SPECIFIC _ARGUMENT_3>`; where SPECIFIC _ARGUMENT_1 is the path to any directory named "*_cnmops" found in data/, SPECIFIC _ARGUMENT_2 is the variable $TARGET_BED found in the `config.mk` file in this module, and SPECIFIC _ARGUMENT_3 is any line printed by the `/bin/create_targets` script in this module.
+## 
+## AUTHOR:
+##      Karla Lozano (klg1219sh@gmail.com), for Winter Genomics (http://www.wintergenomics.com/) - 2018
 
+# Reading arguments from shell
+#
+args = commandArgs(trailingOnly = TRUE)
+# Loading the package cn.mops
+#
 library(cn.mops)
+# Loading BAM files
+#
 BAMFiles <- list.files(path= args[1], pattern=".bam$", full.names = TRUE)
+# Loading BED file
+#
 BEDFile <- read.table(args[2], sep="\t", as.is=TRUE)
+# Setting genomic ranges stablished by the BED file
+#
 genomic_ranges <- GRanges(BEDFile[,1],IRanges(BEDFile[,2],BEDFile[,3]))
-Read_counts <- getSegmentReadCountsFromBAM(BAMFiles,GR=gr)
+# Obtaining read counts form BAM file
+#
+Read_counts <- getSegmentReadCountsFromBAM(BAMFiles,GR=genomic_ranges)
+#Running cn.MOPS algorithm on read counts
+#
 resCNMOPS <- exomecn.mops(Read_counts)
+#Calculating integer copy numbers
+#
 IntegerCN <- calcIntegerCopyNumbers(resCNMOPS)
 CNVs <- as.data.frame(cnvs(IntegerCN))
+#Generating tab delimited file
+#
 write.table(CNVs,file= args[3], quote=FALSE, sep= "\t", row.names=FALSE)
